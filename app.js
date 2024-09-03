e665a0df849c537ed12581a74be5ca365e2af977
Nick Liliakides <nick.liliakides@gmail.com>
2024-09-03 18:00:25 +0100

    fix deployment error 2

--- app.js
+++ app.js
@@ -85,4 +85,14 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-module.exports = app;
+const port = process.env.PORT || 8000;
+
+const server = app.listen(port, '0.0.0.0', () => {
+  console.log(`App is running on port ${port} ...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhadled Rejection! 💥 Shutting down...');
+  server.close(() => process.exit(1));
+});
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const app = require('./app');
+// const port = process.env.PORT || 8000;
 
-const port = process.env.PORT || 8000;
+// const server = app.listen(port, '0.0.0.0', () => {
+//   console.log(`App is running on port ${port} ...`);
+// });
 
-const server = app.listen(port, '0.0.0.0', () => {
-  console.log(`App is running on port ${port} ...`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('Unhadled Rejection! 💥 Shutting down...');
-  server.close(() => process.exit(1));
-});
+// process.on('unhandledRejection', (err) => {
+//   console.log(err.name, err.message);
+//   console.log('Unhadled Rejection! 💥 Shutting down...');
+//   server.close(() => process.exit(1));
+// });
