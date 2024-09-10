1e1981fe5d37505af452e184ce6bacbc10198137
Nick Liliakides <nick.liliakides@gmail.com>
2024-09-10 20:37:05 +0100

    logs for vercel debugging

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const { handleImageUpload } = require('../utils/cloudinaryConfig');
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-  const fileSize = parseInt(req.headers['content-length'], 10);
+  // const fileSize = parseInt(req.headers['content-length'], 10);
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
@@ -17,17 +17,17 @@ const multerFilter = (req, file, cb) => {
       false,
     );
   }
-  if (fileSize < 3000000) {
-    cb(null, true);
-  } else {
-    cb(
-      new AppError(
-        'File is too large. For a profile image, only files smaller than 3MB are accepted.',
-        400,
-      ),
-      false,
-    );
-  }
+  // if (fileSize < 3000000) {
+  //   cb(null, true);
+  // } else {
+  //   cb(
+  //     new AppError(
+  //       'File is too large. For a profile image, only files smaller than 3MB are accepted.',
+  //       400,
+  //     ),
+  //     false,
+  //   );
+  // }
 };
 
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
@@ -58,12 +58,22 @@ exports.getMe = (req, res, next) => {
 exports.uploadUserPhoto = upload.single('photo');
 
 exports.uploadPhotoToCoudinary = catchAsync(async (req, res, next) => {
-  if (!req.file) return next();
+  if (!req.file) next();
+
+  console.log(
+    '🚀 ~ exports.uploadPhotoToCoudinary=catchAsync ~ req.file:',
+    req.file,
+  );
 
   const b64 = Buffer.from(req.file.buffer).toString('base64');
+  console.log('🚀 ~ exports.uploadPhotoToCoudinary=catchAsync ~ b64:', b64);
 
   const dataURI = `data:${req.file.mimetype};base64,${b64}`;
   const cldRes = await handleImageUpload(dataURI);
+  console.log(
+    '🚀 ~ exports.uploadPhotoToCoudinary=catchAsync ~ dataURI:',
+    dataURI,
+  );
   req.file.filename = cldRes.secure_url;
   next();
 });
@@ -81,6 +91,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // Update user document
   const filteredBody = filterObj(req.body, 'name', 'email', 'photo');
+  console.log('🚀 ~ exports.updateMe=catchAsync ~ filteredBody:', filteredBody);
 
   if (req.file) filteredBody.photo = req.file.filename;
   else filteredBody.photo = undefined;
