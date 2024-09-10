dc34cb59fd50ca1af6d6b7877a16d30f8ce2d8e3
Nick Liliakides <nick.liliakides@gmail.com>
2024-09-10 23:50:21 +0100

    remove muter filter

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,30 +7,33 @@ const { handleImageUpload } = require('../utils/cloudinaryConfig');
 
 const multerStorage = multer.memoryStorage();
 
-const multerFilter = (req, file, cb) => {
-  const fileSize = parseInt(req.headers['content-length'], 10);
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    cb(
-      new AppError('File is not an image. Only image files are accepted.', 400),
-      false,
-    );
-  }
-  if (fileSize < 3000000) {
-    cb(null, true);
-  } else {
-    cb(
-      new AppError(
-        'File is too large. For a profile image, only files smaller than 3MB are accepted.',
-        400,
-      ),
-      false,
-    );
-  }
-};
+// const multerFilter = (req, file, cb) => {
+//   const fileSize = parseInt(req.headers['content-length'], 10);
+//   if (file.mimetype.startsWith('image')) {
+//     cb(null, true);
+//   } else {
+//     cb(
+//       new AppError('File is not an image. Only image files are accepted.', 400),
+//       false,
+//     );
+//   }
+//   if (fileSize < 3000000) {
+//     cb(null, true);
+//   } else {
+//     cb(
+//       new AppError(
+//         'File is too large. For a profile image, only files smaller than 3MB are accepted.',
+//         400,
+//       ),
+//       false,
+//     );
+//   }
+// };
 
-const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+const upload = multer({
+  storage: multerStorage,
+  //  fileFilter: multerFilter
+});
 
 const filterObj = (obj, ...allowedFIelds) => {
   const newObj = {};
